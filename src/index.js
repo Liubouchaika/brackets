4c965ac8f8779c3ecce9f95b15e81bc92bf92cf8
Liubou Chaika <liubouchaika@gmail.com>
2020-03-18 17:52:30 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig) { 
+	let arr = bracketsConfig.flat().join('').match(/.{1,2}/g);
+
+	let wasReplaced = true;
+	let count = 0;
+	while (wasReplaced && count < 100) {
+		wasReplaced = false;
+		for (let j=0; j<arr.length; j++) {
+			
+			let initStr = str;
+			str = str.replace(arr[j], '');
+
+			if (initStr != str) {
+				wasReplaced = true;
+			}
+		}
+		count++;
+	}
+
+	return !str;
+}; 
